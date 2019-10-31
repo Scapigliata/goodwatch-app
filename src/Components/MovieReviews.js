93c14c692fcf96f886c688d905e7afc037ac861b
Paulina Ludwig <scapigliataludwig@gmail.com>
2019-10-31 11:55:57 +0100

    FIXED GOOGLE SIGN IN

--- src/Components/MovieReviews.js
+++ src/Components/MovieReviews.js
@@ -58,7 +58,7 @@ const MovieReviews = ({ movies, getMovies }) => {
                 title={title}
               />
             </Card>
-            <div class="flip-card-back"><h1>{title}</h1><hr />
+            <div className="flip-card-back"><h1>{title}</h1><hr />
             From which we spring Rig Veda courage of our questions paroxysm of global death Vangelis star stuff harvesting star light. Inconspicuous motes of rock and gas inconspicuous motes of rock and gas realm of the galaxies rich in heavy atoms at the edge of forever Apollonius of Perga. Extraordinary claims require extraordinary evidence cosmic ocean the sky calls to us hydrogen atoms invent the universe citizens of distant epochs and billions upon billions upon billions upon billions upon billions upon billions upon billions.
             </div>
           </div>
