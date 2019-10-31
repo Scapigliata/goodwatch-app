2f1025c26ed2afe514f2e0bec06d3edf90f51751
Paulina Ludwig <scapigliataludwig@gmail.com>
2019-10-31 17:08:50 +0100

    Added profile route

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,13 @@
 import React from 'react';
+const user = require('../assets/user.webp')
 
 const Profile = () => (
-  <div>
-    Name: "Jane Doe",
-    Reviews: "10"
+  <div className="profile__container">
+    <img src={user} alt="profile__img" className="profile__user"/> 
+    <div className="profile__abstract">
+    <h3>Name: Otter Beaverton</h3>
+    <h3>Reviews: 10</h3>
+    </div>
   </div>
 )
 
